fa62a5bc1723a70e40488c9d794995aa750e20da
Siddharth <course.coder@gmail.com>
2021-12-15 09:37:09 +0530

    map

--- lib/maps/map_demo.dart
+++ lib/maps/map_demo.dart
@@ -1,3 +1,35 @@
+/*
+Dependency
+------------
+google_maps_flutter
+
+
+Step for Writing App
+------------------------
+1) Create the new App
+2) Add the Dependency  'google_maps_flutter'
+3) Create firebase project & connect your app to it
+4) Create Map API key through google developer console & add it to manifest file   
+    https://console.developers.google.com
+5) Enable Maps SDK for Android through google developer console
+
+
+Important classe in google_maps_flutter
+---------------------------------------
+1) GoogleMap
+2) GoogleMapController
+3) LatLng
+4) CameraPosition
+5) CameraUpdate
+6) Marker
+7) InfoWindow
+8) Circle
+9) Polyline
+10) Polygon
+11) BitmapDiscriptor
+
+*/
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
